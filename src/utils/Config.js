0273e1e02e3408c50011de5a7ca0342f143e0b4c
hitesh kanwar <Hitesh.Kanwar@g10x.com>
2022-12-10 23:42:01 -0800

    config js

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,3 +1,6 @@
-export const contactBaseUrl = "https://mza174k33b.execute-api.ca-central-1.amazonaws.com/develop/addContact"
-export const empBaseUrl = "https://eqe15424r8.execute-api.ca-central-1.amazonaws.com/develop/addEmployee"
-export const candBaseUrl = "https://jr9kot0694.execute-api.ca-central-1.amazonaws.com/develop/addCandidate"
+export const contactBaseUrl =
+  "https://mza174k33b.execute-api.ca-central-1.amazonaws.com/develop/addContact";
+export const empBaseUrl =
+  "https://ozgnnmay3k.execute-api.ca-central-1.amazonaws.com/develop/addEmployee";
+export const candBaseUrl =
+  "https://74lc940yoh.execute-api.ca-central-1.amazonaws.com/develop/addCandidate";
